6ac229836056f5be70dbf724162303934e157fe1
amirreza <negahapp@chmail.ir>
2016-01-10 10:25:40 +0000

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'teledark' then -- Put everything you like :)
+  if matches[1]:lower() == 'info1984' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /teledark ")
@@ -120,7 +120,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "teledark" then -- Put everything you like :)
+    if matches[2] == "info1984" then -- Put everything you like :)
       if not is_admin(msg) then
         return "For admins only !"
       else
@@ -141,7 +141,7 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (infobot)",-- Put everything you like :)
+    "^[!/]([Ss]tats) (info1984)",-- Put everything you like :)
     }, 
   run = run
 }
